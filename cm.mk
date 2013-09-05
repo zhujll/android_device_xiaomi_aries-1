5e3d09c34ac569f75cb322e4774f0cb0966f79d2
M1cha <sigmaepsilon92@gmail.com>
2013-09-05 22:17:33 +0200

    cm: use correct screen resolution for bootanimation
    Change-Id: Ibaa48cfbadbd1f9458fae9f263cb35313d4e00e9

--- cm.mk
+++ cm.mk
@@ -1,6 +1,6 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 768
+TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)

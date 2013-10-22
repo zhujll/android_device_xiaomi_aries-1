42b9729c8be44addc2afbb58b9e1db6ee6fe5173
M1cha <sigmaepsilon92@gmail.com>
2013-10-22 17:16:27 +0200

    rename tree [PART 2]
    Change-Id: Idecd7a0599e6b86cd4453099104db777a4d50127

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,12 +36,12 @@ TARGET_NO_BOOTLOADER := true
 
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=aries lpj=67677
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 # Try to build the kernel
 TARGET_KERNEL_SOURCE := kernel/google/msm
-TARGET_KERNEL_CONFIG := cyanogen_mako_defconfig
+TARGET_KERNEL_CONFIG := cyanogen_aries_defconfig
 
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO:= false
@@ -54,11 +54,11 @@ BLUETOOTH_HCI_USE_MCT := true
 
 TARGET_NO_RADIOIMAGE := true
 TARGET_BOARD_PLATFORM := msm8960
-TARGET_BOOTLOADER_BOARD_NAME := MAKO
-TARGET_BOOTLOADER_NAME=mako
-TARGET_BOARD_INFO_FILE := device/lge/mako/board-info.txt
+TARGET_BOOTLOADER_BOARD_NAME := ARIES
+TARGET_BOOTLOADER_NAME=aries
+TARGET_BOARD_INFO_FILE := device/xiaomi/aries/board-info.txt
 
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/mako/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/xiaomi/aries/bluetooth
 
 # Use the CM PowerHAL
 TARGET_USES_CM_POWERHAL := true
@@ -74,7 +74,7 @@ BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_AP  := "ap"
 
-BOARD_EGL_CFG := device/lge/mako/egl.cfg
+BOARD_EGL_CFG := device/xiaomi/aries/egl.cfg
 
 #BOARD_USES_HGL := true
 #BOARD_USES_OVERLAY := true
@@ -85,9 +85,9 @@ TARGET_USES_SF_BYPASS := true
 TARGET_USES_C2D_COMPOSITION := true
 
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_RECOVERY_UI_LIB := librecovery_ui_mako
+TARGET_RECOVERY_UI_LIB := librecovery_ui_aries
 
-TARGET_RECOVERY_FSTAB = device/lge/mako/fstab.mako
+TARGET_RECOVERY_FSTAB = device/xiaomi/aries/fstab.aries
 RECOVERY_FSTAB_VERSION = 2
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 23068672 # 22M
@@ -102,13 +102,13 @@ BOARD_USES_SECURE_SERVICES := true
 BOARD_USES_EXTRA_THERMAL_SENSOR := true
 BOARD_USES_CAMERA_FAST_AUTOFOCUS := true
 
-BOARD_LIB_DUMPSTATE := libdumpstate.mako
+BOARD_LIB_DUMPSTATE := libdumpstate.aries
 
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 TARGET_NO_RPC := true
 TARGET_PROVIDES_GPS_LOC_API := true
 
-TARGET_RELEASETOOLS_EXTENSIONS := device/lge/mako
+TARGET_RELEASETOOLS_EXTENSIONS := device/xiaomi/aries
 
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
@@ -122,12 +122,12 @@ TARGET_QCOM_DISPLAY_VARIANT := caf
 TARGET_QCOM_MEDIA_VARIANT := caf
 TARGET_DISPLAY_USE_RETIRE_FENCE := true
 
--include vendor/lge/mako/BoardConfigVendor.mk
+-include vendor/xiaomi/aries/BoardConfigVendor.mk
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 BOARD_SEPOLICY_DIRS += \
-        device/lge/mako/sepolicy
+        device/xiaomi/aries/sepolicy
 
 BOARD_SEPOLICY_UNION += \
 	file_contexts \
@@ -159,4 +159,4 @@ BOARD_SEPOLICY_UNION += \
 	ueventd.te \
 	wpa_supplicant.te
 
-BOARD_HARDWARE_CLASS := device/lge/mako/cmhw/
+BOARD_HARDWARE_CLASS := device/xiaomi/aries/cmhw/

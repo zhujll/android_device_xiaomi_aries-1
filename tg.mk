f66942ac4fe8aa825acbd1daf4ac2a1e4cc0aa86
Andrew Warfield <hiasant@gmail.com>
2013-11-05 03:18:29 +0100

    Gummy-ify

---
+++ tg.mk
@@ -0,0 +1,23 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/Gummy/config/gsm.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 768
+
+$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
+
+$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
+
+$(call inherit-product, device/lge/mako/full_mako.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := mako
+PRODUCT_NAME := tg_mako
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 4
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch

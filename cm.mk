11ec39b4694c10a470d5de2c632fd8e60bee3510
M1cha <sigmaepsilon92@gmail.com>
2014-05-03 00:15:58 +0200

    remove NFC support
    Change-Id: Ib659fe3db35ccad9a7715d4491c2ad322d377b07

--- cm.mk
+++ cm.mk
@@ -3,9 +3,6 @@ $(call inherit-product, device/xiaomi/aries/full_aries.mk)
 # Inherit some common CM stuff
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 

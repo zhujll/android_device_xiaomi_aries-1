3562cc0fbbbd8eaa88bad559292ebd804fc468d5
M1cha <sigmaepsilon92@gmail.com>
2014-05-03 01:04:48 +0200

    remove bootloader image
    Change-Id: I1be3cbb553ea4801a98250f7e4c52dba870767e6

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,7 +4,3 @@ LOCAL_PATH := $(call my-dir)
 # extra images
 #----------------------------------------------------------------------
 include build/core/generate_extra_images.mk
-
-# Radio images
-$(call add-radio-file,radio/filesmap)
-$(call add-radio-file,radio/emmc_appsboot.mbn)

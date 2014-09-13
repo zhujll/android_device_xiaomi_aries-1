a2dc541f59539865c3e4663aff637082aee9b8c2
M1cha <sigmaepsilon92@gmail.com>
2014-09-13 14:18:19 +0200

    Revert "Add mako's audio package"
    This reverts commit 6c97d939a5ba8ce90722ec95208ce19ed7cd111e.
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -288,7 +288,5 @@ PRODUCT_PACKAGES += qrngd
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
-# This is the aries-specific audio package
-$(call inherit-product, frameworks/base/data/sounds/AudioPackage10.mk)
 
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)

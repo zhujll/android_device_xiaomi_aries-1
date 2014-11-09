8f3c840ac056e72e561c87fffc7521c005be6891
M1cha <sigmaepsilon92@gmail.com>
2014-11-09 16:27:16 +0100

    fix RIL
    Change-Id: I26c081f131023a0defd4f230564d08dfaec5d585

--- extract-files.sh
+++ extract-files.sh
@@ -24,7 +24,8 @@ fi
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
 
-for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ | grep -v ^-`; do
+for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$`; do
+    FILE=`echo $FILE | sed -e "s/^-//g"`
     DIR=`dirname $FILE`
     if [ ! -d $BASE/$DIR ]; then
         mkdir -p $BASE/$DIR

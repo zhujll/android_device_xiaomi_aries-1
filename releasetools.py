M1cha/android_device_xiaomi_aries

add syspart selector
Change-Id: I37e38dac91beb82bfc86ae3566cb51c0ba653959

releasetools.py
@@ -195,3 +195,6 @@ def trunc_to_null(s):
     return s[:s.index('\0')]
   else:
     return s
+
+def FullOTA_InstallBegin(info):
+	info.script.AppendExtra('run_program("/sbin/syspart_select", "switch");');

zhujll/android_device_xiaomi_aries-1

releasetools.py
@@ -195,3 +195,6 @@ def trunc_to_null(s):
     return s[:s.index('\0')]
   else:
     return s
+
+def FullOTA_PostBackup(info):
+	info.script.AppendExtra('run_program("/sbin/syspart_select", "switch");');
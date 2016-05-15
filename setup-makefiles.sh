55e4c071acca22f4e9dd00858ad25b257cde2e74
sndnvaps <sndnvaps@gmail.com>
2016-05-15 19:16:04 +0800

    add missing file -> libtime_genoff.so
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -91,5 +91,15 @@ LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include \$(BUILD_PREBUILT)
 
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := libtime_genoff
+LOCAL_MODULE_OWNER := qcom
+LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include \$(BUILD_PREBUILT)
+
 endif
 EOF

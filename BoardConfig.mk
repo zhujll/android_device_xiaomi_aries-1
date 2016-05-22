c3ffcf146a6933671569c78269cabf1a70d4478e
sndnvaps <sndnvaps@gmail.com>
2016-05-22 22:32:12 +0800

    update for caf
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,16 +133,16 @@ BOARD_RIL_NO_CELLINFOLIST := true
 TARGET_RIL_VARIANT := caf
 #BOARD_PROVIDES_LIBRIL := true
 
-#TARGET_QCOM_DISPLAY_VARIANT := caf
+TARGET_QCOM_DISPLAY_VARIANT := caf
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/xiaomi/aries
 
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk
 
-#BOARD_SEPOLICY_DIRS += \
-#    device/xiaomi/aries/sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/xiaomi/aries/sepolicy
 
 BOARD_CHARGER_ENABLE_SUSPEND := true
 

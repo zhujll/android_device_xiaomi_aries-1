7193228a7c36d1d150b24fb66807f57dc2d8b111
sndnvaps <sndnvaps@gmail.com>
2016-06-29 18:23:51 +0800

    multirom: add mrom_infos for multirom support
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- mrom.mk
+++ mrom.mk
@@ -8,7 +8,7 @@ MR_DPI := hdpi
 MR_DPI_FONT := 216
 MR_FSTAB := device/xiaomi/aries/mrom/twrp.fstab
 MR_KEXEC_MEM_MIN := 0x85000000
-#MR_INFOS := device/lge/mako/mrom_infos
+MR_INFOS := device/xiaomi/aries/mrom_infos
 MR_PIXEL_FORMAT := "RGBX_8888"
 CMDLINE_FOR_HARDBOOT := console=ttyHSL0,115200,n8 androidboot.hardware=aries lpj=67677 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 lge.kcal=0|0|0|x root=mmcblk0
 # ./kexec --load-hardboot ../zImage --mem-min=0x85000000 --atags-addr=0x80200100 --command-line='console=ttyHSL0,115200,n8 androidboot.hardware=aries lpj=67677 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 lge.kcal=0|0|0|x root=mmcblk0'

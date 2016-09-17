99e9b575749654eff9a69443ba6f65b6f3c34950
werty100 <pablo-zzr@hotmail.com>
2016-09-17 15:57:36 +0200

    Aries 1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,7 +15,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/aosp_aries.mk \
-	$(LOCAL_DIR)/full_aries.mk \
-	$(LOCAL_DIR)/mk_aries.mk \
-	$(LOCAL_DIR)/cm.mk
+    $(LOCAL_DIR)/aosp_aries.mk \
+    $(LOCAL_DIR)/full_aries.mk
